567b470df7f281cbf7c441a3ed74482d02eb5c2a
Marius Fylling <marius@sdok.no>
2023-02-11 02:06:25 +0000

    First iteration of website

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/*"],
+  theme: {
+    fontFamily: {
+      'display': ['Druk'],
+      'sans': ['Druk'],
+    },
+    colors: {
+      'dsb': '#2E3F5C',
+      'white': '#FFFFFF',
+      'gray': '#DADADA',
+      'orange': '#FC5205',
+      'dark': '#1F1F1F',
+      'black': '#000000',
+      'gray2': '#707070',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
